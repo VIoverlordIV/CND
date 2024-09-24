641ae3d699efa509f706fc98e8a791611bbd274a
VIoverlordIV <qq1295490152@gmail.com>
2024-09-24 10:57:19 +0900

    Initial Commit

---
+++ CND/CNDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CNDApp.swift
+//  CND
+//
+//  Created by cmStudent on 2024/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CNDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BrickBreakingGame()
+        }
+    }
+}
